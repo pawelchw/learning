29312dab856a92bab86de2c0814daeb595ba8268
pawel <pawel-dell@pawel.(none)>
2016-05-06 05:58:28 +0100

    we have fixed two and three model comparison, and it shows the right estimation for data generated by 3 different Poisson distribution. we have added different main functions to have a possibility of working with different output. These should be improved later

--- code/count_data/count_data_analysis/count_data_analysis.py
+++ code/count_data/count_data_analysis/count_data_analysis.py
@@ -14,7 +14,8 @@ colors = ['#348ABD', '#A60628', '#7A68A6', '#467821', '#D55E00',
           '#CC79A7', '#56B4E9', '#009E73', '#F0E442', '#0072B2']
 
 
-M=10000
+M=50000
+B = 5000
 g_df = 0
 def load_csv( path="aa" ):
    #accidents = df.ix[:,1]
@@ -26,18 +27,35 @@ def plt_comparison( p_data
                   , p_x_label
                   , p_y_label
                   , p_title
+                  , p_add_mu = 'n'
                   ):
   global colors
   fig = plt.figure(figsize=(10,6))
   x_min = np.min(p_x)
   x_max = np.max(p_x)
+  l_start = 0
+  
+  if p_add_mu == 'y':
+      l_start=1
+      fig.add_subplot( int(str(len(p_data)+l_start) + '11' ) )
+      x = [i for i in xrange(len(p_data[0]))]
+      plt.plot(x,p_data[0])
+
+      for i in xrange(1,len(p_data)):
+
+        y=np.mean(p_data[i])
+        y_seq = [y for s in x]
+        plt.plot(x,y_seq)
+
+      
+
   for i in xrange( len(p_data) ):
 
     # add stacked charts
     # this many charts - int(str(len(z))
     # this many columns - '1'
     # this exactly char - str(i+1)
-    fig.add_subplot( int(str(len(p_data)) + '1' + str(i+1) ) )
+    fig.add_subplot( int(str(len(p_data)+l_start) + '1' + str(l_start+i+1) ) )
     plt.hist(p_data[i], range=[x_min, x_max], bins=100, histtype='stepfilled', color=colors[1])   
     plt.xlim(x_min, x_max)
     plt.xlabel( p_x_label[i] )
@@ -69,10 +87,10 @@ def max_a_posteriori( p_model ):
    ma.fit()
    return ma
 
-def mcmc_estimation( N, p_model ):
+def mcmc_estimation( N,B, p_model ):
 
    mcmc = pm.MCMC(p_model)
-   mc_res = mcmc.sample(N,100)
+   mc_res = mcmc.sample(N,B)
    return mcmc
 
 def negative_b_mcm_model( p_df ):
@@ -89,13 +107,15 @@ def negative_b_mcm_model( p_df ):
 def two_model_comparison( p_df ):
 
    a_n = len(p_df)
-   d_lam = pm.Uniform('d_uni_alpha',0,1)
-   #lambda_1 = pm.Exponential("lambda_1", d_lam)
-   lambda_1 = pm.Exponential("lambda_1", d_lam)   
+   d_lam = pm.Uniform('d_lam',0,1)
+   #d_lam = 1.0 / np.mean(p_df)
+   lambda_1 = pm.Exponential("lambda_1", d_lam)
+   #lambda_1 = pm.Uniform("lambda_1", min(p_df), max(p_df))
    lambda_2 = pm.Exponential("lambda_2", d_lam)
+   #lambda_2 = pm.Uniform("lambda_2",min(p_df), max(p_df))
 
-   tau = pm.DiscreteUniform("tau", lower=min(p_df), upper=max(p_df) )
-
+   #tau = pm.DiscreteUniform("tau", lower=min(p_df), upper=max(p_df) )
+   tau = pm.DiscreteUniform("tau", lower=0, upper=max(p_df) )
    @pm.deterministic
    def lambda_(tau=tau, lambda_1=lambda_1, lambda_2=lambda_2):
       out = np.zeros(a_n)
@@ -103,31 +123,87 @@ def two_model_comparison( p_df ):
       out[tau:] = lambda_2  # lambda after (and including) tau is lambda2
       return out
 
-   #d_obs = pm.NegativeBinomial('d_observed', mu=lambda_, alpha = d_alpha, value=p_df, observed=True)
    d_obs = pm.Poisson('d_observed', mu=lambda_, value=p_df, observed=True)
 
    d_model = pm.Model([d_obs, d_lam, lambda_1, lambda_2, tau])
+   #d_model = pm.Model([d_obs,  lambda_1, lambda_2, tau])
 
    return d_model, d_obs, d_lam, lambda_1, lambda_2, tau
 
+def three_model_comparison( p_df ):
+
+   a_n = len(p_df)
+   t_lam = pm.Uniform('d_lam',0,1)
+   #d_lam = 1.0 / np.mean(p_df)
+   t_lambda_1 = pm.Exponential("t_lambda_1", t_lam)
+   #t_lambda_1 = pm.Uniform("t_lambda_1", min(p_df), max(p_df))
+   t_lambda_2 = pm.Exponential("t_lambda_2", t_lam)
+   #t_lambda_2 = pm.Uniform("t_lambda_2",min(p_df), max(p_df))
+   t_lambda_3 = pm.Exponential("t_lambda_3", t_lam)
+   #t_lambda_2 = pm.Uniform("t_lambda_2",min(p_df), max(p_df))
+
+   #tau = pm.DiscreteUniform("tau", lower=min(p_df), upper=max(p_df) )
+   t_tau_1 = pm.DiscreteUniform("tau1", lower=0, upper=max(p_df)-1 )
+   t_tau_2 = pm.DiscreteUniform("tau", lower=t_tau_1, upper=max(p_df) )
+   @pm.deterministic
+   def lambda_(tau_1=t_tau_1,tau_2=t_tau_2, lambda_1=t_lambda_1, lambda_2=t_lambda_2, lambda_3=t_lambda_3):
+      out = np.zeros(a_n)
+      out[:tau_1] = lambda_1  # lambda before tau_1 is lambda1
+      out[tau_1:tau_2] = lambda_2  # lambda_2 between tau_1 and tau_2
+      out[tau_2:] = lambda_3  # lambda after (and including) tau is lambda_3
+      return out
+
+   t_obs = pm.Poisson('t_observed', mu=lambda_, value=p_df, observed=True)
+
+   t_model = pm.Model([t_obs, t_lam, t_lambda_1, t_lambda_2, t_lambda_3, t_tau_1, t_tau_2])
+   #d_model = pm.Model([d_obs,  t_lambda_1, t_lambda_2, tau])
+
+   return t_model, t_lam, t_lambda_1, t_lambda_2, t_lambda_3, t_tau_1, t_tau_2
 
 def main():
 
-   global g_df, M
+   global g_df, M, B
    g_df=load_csv("aa")
 
    ml = max_likelihood()
    s_mu ,s_ob ,s_es ,s_model = simple_mcmc_model( g_df )
    s_map = max_a_posteriori( s_model )
-   s_mcmc = mcmc_estimation( M, s_model )
+   s_mcmc = mcmc_estimation( M, B, s_model )
 
 
    n_mu, n_lam, n_alpha, n_ob, n_es, n_model = negative_b_mcm_model( g_df )
-   n_mcmc = mcmc_estimation( M, n_model )
+   n_mcmc = mcmc_estimation( M, B, n_model )
 
    d_model, d_obs, d_lam, lambda_1, lambda_2, tau = two_model_comparison( g_df )
-   d_mcmc = mcmc_estimation( M, d_model )
+   d_mcmc = mcmc_estimation( M, B, d_model )
+
+   t_model, t_lam, t_lambda_1, t_lambda_2, t_lambda_3, t_tau_1, t_tau_2= three_model_comparison( g_df )
+   t_mcmc = mcmc_estimation( M, B, t_model )
+
 
    return s_mu, s_ob, s_es, s_model, s_map,  ml, g_df, s_mcmc, n_mu, n_lam, n_alpha, n_ob, n_es, n_model, n_mcmc\
-        , d_model, d_obs, d_lam, lambda_1, lambda_2, tau, d_mcmc
+        , d_model, d_obs, d_lam, lambda_1, lambda_2, tau, d_mcmc, t_model, t_lam, t_lambda_1, t_lambda_2\
+        , t_lambda_3, t_tau_1, t_tau_2, t_mcmc
+
+
+def d_main():
+
+   global g_df, M, B
+   g_df=load_csv("aa")
+
+   d_model, d_obs, d_lam, lambda_1, lambda_2, tau = two_model_comparison( g_df )
+   d_mcmc = mcmc_estimation( M, B, d_model )
+
+   return d_model, d_obs, d_lam, lambda_1, lambda_2, tau, d_mcmc, g_df
+
+
+def t_main():
+
+   global g_df, M, B
+   g_df=load_csv("aa")
+
+   t_model, t_lam, t_lambda_1, t_lambda_2, t_lambda_3, t_tau_1, t_tau_2= three_model_comparison( g_df )
+   t_mcmc = mcmc_estimation( M, B, t_model )
+
+   return t_model, t_lam, t_lambda_1, t_lambda_2, t_lambda_3, t_tau_1, t_tau_2, t_mcmc, g_df
 
