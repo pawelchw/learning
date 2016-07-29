a7789c548821face0cd9c012f5d9d3056ad2e77a
pawel <pawel-dell@pawel.(none)>
2016-07-29 22:59:46 +0100

    pushing changes as of 20160729

--- ji/mon.py
+++ ji/mon.py
@@ -1,7 +1,19 @@
+from __future__ import print_function
 import numpy as np
 import pandas as pd
+import csv
+import string
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import pdb
 
+'''
+proper print out
+>>> for i in tr['Agent'].unique() :
+...     ''.join(['if df[\'Agent\'] ==\'',i,'\':','df[\'',i,'\']==1'])
+'''
 
-df_t = pd.read_csv("/home/pawel-dell/learning/cv_pack/mv/txn_data.csv")
-df_u = pd.read_csv("/home/pawel-dell/learning/cv_pack/mv/user_data.csv")
-df_d = pd.read_csv("/home/pawel-dell/learning/cv_pack/mv/txn_data.csv")
+p = pd.read_csv("/home/pawel-dell/learning/cv_pack/apes_master/promo_lookup.csv")
+t = pd.read_csv("/home/pawel-dell/learning/cv_pack/apes_master/txn_data.csv")
+u = pd.read_csv("/home/pawel-dell/learning/cv_pack/apes_master/user_data.csv")
+d = pd.read_csv("/home/pawel-dell/learning/cv_pack/apes_master/user_day_data.csv")
